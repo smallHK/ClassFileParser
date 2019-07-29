e527b3dd1996c52911988a4561b2a6c04b873146
smallHK <30528205+smallHK@users.noreply.github.com>
2019-07-29 13:51:57 +0800

    gradle

---
+++ src/main/java/com/hk/TestGradle.java
@@ -0,0 +1,10 @@
+
+
+package com.hk;
+public class TestGradle {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println(System.getProperty("user.dir"));
+	}
+}
